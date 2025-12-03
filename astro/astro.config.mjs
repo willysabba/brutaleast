470f57c7a1b157b9c7509836175ff0347f4088a4
guillermo.sabbatella <guillermo.sabbatella@avature.net>
2025-12-04 00:56:48 +0100

    Config Astro para multi-entorno

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: process.env.SITE_URL || 'http://localhost:4321',
+  base: process.env.PUBLIC_BASE || '/',
+  integrations: [mdx(), sitemap()],
 });
